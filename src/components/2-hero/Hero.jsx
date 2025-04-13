8f92dc3c4c93cd4b638c47f49819dfcc1bcde66c
NizarNaser <nizarnaser17@gmail.com>
2025-04-13 18:53:24 +0200

    Deploy updated build

---
+++ src/components/2-hero/Hero.jsx
@@ -0,0 +1,64 @@
+import Lottie from "lottie-react";
+import "./hero.css";
+import devAnimation from "../../animation/dev.json";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+
+const Hero = () => {
+  const lottieRef = useRef();
+
+  return (
+    <section className="hero flex">
+      <div className="left-section  ">
+        <div className="parent-avatar flex">
+          <motion.img
+            initial={{ transform: "scale(0)" }}
+            animate={{ transform: "scale(1.1)" }}
+            transition={{ damping: 6, type: "spring", stiffness: 100 }}
+            src="./nizar.png"
+            className="avatar"
+            alt=""
+          />
+          <div className="icon-verified"></div>
+        </div>
+
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className="title"
+        >
+          Software Developer and Digital Marketing Specialist
+        </motion.h1>
+
+        <p className="sub-title">
+        My name is Nizar Nasser, and I am a freelance software developer with a strong command of multiple programming languages including Java, JavaScript, Python, PHP, as well as styling languages. I specialize in building both web and desktop applications, and I have hands-on experience working with popular frameworks such as WordPress and Laravel. In addition to software development, I am also involved in digital marketing and SEO, providing comprehensive solutions to help businesses grow and succeed online.
+
+
+        </p>
+
+        <div className="all-icons flex">
+          <div className="icon icon-twitter"></div>
+          <div className="icon icon-instagram"></div>
+          <div className="icon icon-github"></div>
+          <div className="icon icon-linkedin"></div>
+        </div>
+      </div>
+
+      <div className="right-section animation ">
+        <Lottie
+          lottieRef={lottieRef}
+          className=""
+          onLoadedImages={() => {
+            // @ts-ignore
+            // https://lottiereact.com/
+            lottieRef.current.setSpeed(0.5);
+          }}
+          animationData={devAnimation}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
